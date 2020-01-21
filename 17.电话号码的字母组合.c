89e00c2edb187f7d31453d46b3dd6f48366cb67a
prcv7on <32297752+prcv7on@users.noreply.github.com>
2020-01-21 17:27:05 +0800

    Update 17.电话号码的字母组合.c

--- 17.电话号码的字母组合.c
+++ 17.电话号码的字母组合.c
@@ -6,45 +6,9 @@
 
 // @lc code=start
 
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int csize(char c) {
-    if (c=='7' || c=='9') return 4;
-    return 3;
-}
-
-char** letterCombinations(char* digits, int* sz) {
-    int l=strlen(digits);
-    if (!l) {
-        *sz=0;
-        return NULL;
-    }
-    int i,j,k;
-    char num[8][5]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    
-    *sz=1;
-    for (i=0;i<l;++i) {
-        *sz *= csize(digits[i]);
-    }
-
-    char** res = (char**)malloc(*sz * sizeof(char*));
-    for (i=0;i<(*sz);++i) {
-        res[i]=(char*)malloc((l+1)*sizeof(char));
-        res[i][l]='\0';
-    }
-
-    k=*sz;
-    for (i=0;i<l;++i) {
-        k /= csize(digits[i]);
-        for (j=0;j<(*sz);++j) {
-            res[j][i]=num[digits[i]-'2'][(j/k)%csize(digits[i])];
-        }
-    }
-    return res;
-}
-
 
 // @lc code=end
 
@@ -95,6 +59,5 @@ int main() {
         printf("%s \n",ret[i]);
     }
     printf("\n");
-
 
 }
